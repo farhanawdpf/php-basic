4d25adfda85d527a17d79eed3ae2255ce28cbf8a
farhanawdpf <farhanawdpf@gmail.com>
2025-06-25 09:16:40 +0600

    df

--- array/array_type/multidimensional_arrays.php
+++ array/array_type/multidimensional_arrays.php
@@ -1,11 +1,28 @@
 
 <?php 
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+echo $cars[3][2];
 
-$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
-var_dump($car);
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+    for ($col = 0; $col < 3; $col++) {
+      echo "<li>".$cars[$row][$col]."</li>";
+    }
+  echo "</ul>";
+}
 
 
-$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
-echo $car["model"];
+for ($row = 0; $row < 3; $row++) {
 
+    for ($col = 0; $col < 3; $col++) {
+      echo "<li>".$cars[$row][$col]."</li>";
+    }
+  echo "</ul>";
+}
 ?>
