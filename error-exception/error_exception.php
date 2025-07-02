b1466859cf5f834643ff4fb6164d98ca8ba2392a
farhanawdpf <farhanawdpf@gmail.com>
2025-07-02 11:29:17 +0600

    update

--- error-exception/error_exception.php
+++ error-exception/error_exception.php
@@ -1,5 +1,17 @@
 <?php
+// Definition and Usage
+// The Exception() constructor is used to create an Exception object and set some of its properties.
 
+// Syntax
+// new Exception(message, code, previous)
+// getMessage — Gets the Exception message
+
+
+// Definition and Usage
+// The getMessage() method returns a description of the error or behaviour that caused the exception to be thrown.
+
+// Syntax
+// $exception->getMessage()
 // try {
 //     tryStatements
 //   } catch (exceptionVar) {
@@ -8,15 +20,21 @@
 //     finallyStatements
 // }
 
-
-$a = 40;
+// <?php
+// try {
+//     throw new Exception("Some error message");
+// } catch(Exception $e) {
+//     echo $e->getMessage();
+// }
+// 
+$a = 10;
 
 
 try{ 
 if ($a<30){ 
- throw new Exception("Invalid Number"); 
+ throw new Exception("valid Number"); 
 }else { 
-    throw new Exception("Valid Number");
+    throw new Exception("invalid Number");
 }
 }
 catch(Exception $e){ 
--- error-exception/php-error.php
+++ error-exception/php-error.php
@@ -1,22 +1,37 @@
-<?php 
+<h1>
+<pre> 
+Types of PHP Errors
+There are primarily four types of errors in PHP - 
 
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-//perse error(syntax error) and fatal error(programming eroor)
+1.Fatal run-time errors
+2.Warning errors
+3.Parse errors (Syntax errors)
+4.Notice errors
+</pre>
+</h1>
+
+
+<?php 
+//perse error(syntax error) and fatal error(programming error),E_NOTICE(Run-time notices. The script found something that might be an error, but could also happen when running a script normally(variable not generate))
 
 // function info(){ 
 //  echo "done";
 // }
 
-echo info();
+// echo info();
 
+// tomorrow someone adds this
+// function show(){ 
+//     return "hello";
+// }
+// echo "hi"//Parse error: syntax error
+// echo show();//Fatal error:
 
-
+// include('header.php');//warring
+        if(isset($n)){ 
+            return $a=$n+2;
+        }else{ 
+            trigger_error('$n is notice error');//notice error
+        }
 
 ?>
